5c815f664adad0c6ecf8ad0048a512ccf708e956
Lupillo23 <126260065+Lupillo23@users.noreply.github.com>
2025-04-29 14:24:56 -0600

    Add files via upload

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "test";
+
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+if (!$conn) {
+    die("No hay conexión: " . mysqli_connect_error());
+}
+
+$nombre = $_POST["txtusuario"];
+$pass = $_POST["txtpassword"];
+
+$query = mysqli_query($conn, "SELECT * FROM login WHERE usuario = '" . $nombre . "' AND password = '" . $pass . "'");
+$nr = mysqli_num_rows($query);
+
+if ($nr == 1) {
+    // header("Location: pagina.html");
+    echo "Bienvenido: " . $nombre;
+} else if ($nr == 0) {
+    echo "No ingreso";
+}
+?>
